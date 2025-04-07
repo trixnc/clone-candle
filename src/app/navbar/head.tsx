94b078fc3c3f24ed090881d372fedac8a717ac1c
Khangai <nkhangain@gmail.com>
2025-04-07 16:09:33 +0800

    Container

---
+++ src/app/navbar/head.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Head = () => {
+    return (
+        <nav>
+            <header>
+                <h1>My Website</h1>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
+            </header>
+        </nav>
+    );
+};
+
+export default Head;
